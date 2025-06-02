c9ba76c9222b58121b238226e01384e69afa50a4
Arman <aaproyan@bk.ru>
2025-06-02 16:53:51 +0400

    delete uuid

--- src/components/NavBar/Navbar.const.ts
+++ src/components/NavBar/Navbar.const.ts
@@ -1,11 +1,10 @@
 import { MenuTitles, TNavBar } from './NavBar.types'
-import { v4 as uuid } from 'uuid'
 
 export const NavBarItems: TNavBar<string | number>[] = [
-  { id: uuid(), title: MenuTitles.HOME, isActive: true },
-  { id: uuid(), title: MenuTitles.SCHEDULE, isActive: false },
-  { id: uuid(), title: MenuTitles.SPEAKERS, isActive: false },
-  { id: uuid(), title: MenuTitles.TICKETS, isActive: false },
-  { id: uuid(), title: MenuTitles.VENUE, isActive: false },
-  { id: uuid(), title: MenuTitles.SPONSORS, isActive: false }
+  { id: 1, title: MenuTitles.HOME, isActive: true },
+  { id: 2, title: MenuTitles.SCHEDULE, isActive: false },
+  { id: 3, title: MenuTitles.SPEAKERS, isActive: false },
+  { id: 4, title: MenuTitles.TICKETS, isActive: false },
+  { id: 5, title: MenuTitles.VENUE, isActive: false },
+  { id: 5, title: MenuTitles.SPONSORS, isActive: false }
 ]
